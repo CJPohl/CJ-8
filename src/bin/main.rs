b0a57fdad2d3bb69af4a6c5ed73e6c8fc204a37c
Christopher J. Pohl <cjpohldev@gmail.com>
2022-07-11 16:21:23 -0400

    impl cartridge and basic opcodes

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,35 +1,51 @@
+use std::env;
+
 use cj_8::chip_8::Platform;
 use cj_8::chip_8::System;
 use cj_8::chip_8::AU;
+use cj_8::chip_8::CU;
 use cj_8::chip_8::GU;
 use cj_8::chip_8::KU;
 
 fn main() {
+    // Accept args and throw errors if necessary
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("ERROR: Failed to parse args");
+        panic!("usage: cj-8 resolution-scale path-to-ROM");
+    }
+    println!("Args accepted");
+
+    let scale = args[1].parse::<u32>().unwrap();
+
+    // Init cartridge unit
+    let cartridge = CU::new(&args[2]).unwrap();
+
     // Init blank slate system
     let mut system = System::new();
-    print!("New CJ-8 created...\n");
+    println!("New CJ-8 created with cartridge");
 
     // Setup render system and input
     const WINDOW_WIDTH: u32 = 64;
     const WINDOW_HEIGHT: u32 = 32;
     let context = Platform::new();
-    println!("SDL context created...");
+    println!("SDL context created");
 
     let mut graphical_unit = GU::new(
         &context.context,
-        "Chip-8 In Rust!",
-        WINDOW_WIDTH * 20,
-        WINDOW_HEIGHT * 20,
+        "CJ-8",
+        WINDOW_WIDTH * scale,
+        WINDOW_HEIGHT * scale,
         // 15,
         // 15,
     );
     let mut keyboard_unit = KU::new(&context.context);
     let audio_unit = AU::new(&context.context);
     graphical_unit.init();
-    println!("Front-End Units Initialized...");
+    println!("Front-End Units Initialized");
 
     // Clear memory and load ROM
-    system.init();
+    system.init(cartridge.buffer);
 
     // Emu loop
     let mut quit = false;
@@ -37,7 +53,12 @@ fn main() {
         // Store keypress state
         quit = keyboard_unit.process_input();
 
-        // beep
-        audio_unit.device.resume();
+        // Program Cycle
+        system.emulate_cycle(&audio_unit);
+
+        // Check drawflag
+        if system.draw_flag == true {
+            // graphical_unit::draw(&system.gfx);
+        }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,20 @@ pub mod chip_8 {
     use sdl2::event::Event;
     use sdl2::keyboard::Keycode;
     use sdl2::{render::Canvas, EventPump, Sdl};
+    use std::fs;
 
     pub struct System {
         opcode: u16,
         v: [u8; 16],
-        i: u16,
+        i: usize,
         pc: u16,
         delay_timer: u8,
         sound_timer: u8,
         stack: [u16; 16],
-        sp: u16,
+        sp: usize,
         memory: [u8; 4096],
-        gfx: [u8; 64 * 32],
+        gfx: [u16; 64 * 32],
+        pub draw_flag: bool,
         key: [u8; 16],
         font_set: [u8; 80],
     }
@@ -24,7 +26,7 @@ pub mod chip_8 {
     impl System {
         pub fn new() -> System {
             System {
-                opcode: 0,
+                opcode: 0x000,
                 v: [0x0; 16],
                 i: 0,
                 pc: 0x200,
@@ -33,7 +35,8 @@ pub mod chip_8 {
                 stack: [0x000; 16],
                 sp: 0,
                 memory: [0x0; 4096],
-                gfx: [0x0; 64 * 32],
+                gfx: [0x000; 64 * 32],
+                draw_flag: false,
                 key: [0x0; 16],
                 font_set: [
                     0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70, 0xF0, 0x10, 0xF0,
@@ -47,19 +50,164 @@ pub mod chip_8 {
             }
         }
 
-        pub fn init(&mut self) {
+        pub fn init(&mut self, buffer: Vec<u8>) {
             // Load fontset
             for (i, font) in self.font_set.iter().enumerate() {
                 self.memory[i] = *font;
             }
+            println!("Font loaded");
 
             // Load ROM into memory at address 0x200
-            // for (i, byte) in buffer.iter().enumerate() {
-            //              self.memory[i+512] = *byte
-            // }
+            for (i, byte) in buffer.iter().enumerate() {
+                self.memory[i + 512] = *byte
+            }
+            println!("Cartridge loaded successfully");
         }
 
-        pub fn emulate_cycle() {}
+        // Test hardcoded opcodes
+        pub fn emulate_cycle(&mut self, audio_unit: &AU) {
+            // Fetch
+            let index = self.pc as usize;
+            self.opcode = u16::from(self.memory[index]) << 8 | u16::from(self.memory[index + 1]);
+            let op_index = self.opcode as usize;
+
+            println!("Executing: {:#x}", self.opcode);
+
+            // Decode
+            match self.opcode & 0xF000 {
+                0x0000 => {
+                    match self.opcode & 0x000F {
+                        // Clears the screen
+                        0x0000 => {
+                            // TBD
+                        }
+                        // Returns from subroutine
+                        0x000E => {
+                            // TBD
+                        }
+                        _ => {
+                            panic!("ERROR: Unknown opcode: {:#X}", self.opcode);
+                        }
+                    }
+                }
+
+                // Calls the subroutine at address NNN
+                0x2000 => {
+                    self.stack[self.sp] = self.pc;
+                    self.sp += 1;
+                    self.pc = self.opcode & 0x0FFF;
+                }
+                // Sets VX to NN
+                0x6000 => {
+                    self.v[(op_index & 0x0F00) >> 8] = (self.opcode & 0x00FF) as u8;
+                    self.pc += 2;
+                }
+                0x8000 => {
+                    match self.opcode & 0x0FFF {
+                        // Adds the value of register VY to VX
+                        0x0004 => {
+                            // If sum is larger than 255 set carry flag
+                            if self.v[(op_index & 0x00F0) >> 4]
+                                > (0xFF - self.v[(op_index & 0x0F00) >> 8])
+                            {
+                                self.v[0xF] = 1;
+                            } else {
+                                self.v[0xF] = 0;
+                            }
+                            self.v[(op_index & 0x0F00) >> 8] += self.v[(op_index & 0x00F0) >> 4];
+                            self.pc += 2;
+                        }
+                        _ => {
+                            panic!("ERROR: Unknown opcode: {:#X}", self.opcode);
+                        }
+                    }
+                }
+                // Sets i to the address NNN
+                0xA000 => {
+                    // Execute
+                    self.i = (self.opcode & 0x0FFF) as usize;
+                    self.pc += 2;
+                }
+                // Draw a sprite at coord (VX, VY)
+                0xD000 => {
+                    let x = u16::from(self.v[(op_index & 0x0F00) >> 8]);
+                    let y = u16::from(self.v[(op_index & 0x00F0) >> 4]);
+                    let height = self.opcode & 0x000F;
+                    let mut pixel: u16;
+
+                    // Reset VF
+                    self.v[0xF] = 0;
+
+                    // Loop over rows
+                    for yline in 0..height {
+                        pixel = u16::from(self.memory[self.i + yline as usize]);
+
+                        // Loop over columns
+                        for xline in 0..8 {
+                            if (pixel & (0x80 >> xline)) != 0 {
+                                if self.gfx[(x + xline + ((y + yline) * 64)) as usize] == 1 {
+                                    self.v[0xF] = 1;
+                                }
+                                self.gfx[(x + xline + ((y + yline) * 64)) as usize] ^= 1;
+                            }
+                        }
+                    }
+
+                    self.draw_flag = true;
+                    self.pc += 2;
+                }
+                0xE000 => {
+                    match self.opcode & 0x00FF {
+                        // For 9E: Skips next instruction if key stored in VX is pressed
+                        0x009E => {
+                            if self.key[self.v[(op_index & 0x0F00) >> 8] as usize] != 0 {
+                                self.pc += 4;
+                            } else {
+                                self.pc += 2;
+                            }
+                        }
+                        _ => {
+                            panic!("ERROR: Unknown opcode: {:#X}", self.opcode);
+                        }
+                    }
+                }
+
+                0xF000 => {
+                    match self.opcode & 0x00FF {
+                        // Stores binary decimal representation of VX at address i, i + 1, and i + 2
+                        0x0033 => {
+                            self.memory[self.i] = self.v[(op_index & 0x0F00) >> 8] / 100;
+                            self.memory[self.i + 1] = (self.v[(op_index & 0x0F00) >> 8] / 10) % 10;
+                            self.memory[self.i + 2] = (self.v[(op_index & 0x0F00) >> 8] % 100) % 10;
+                            self.pc += 2;
+                        }
+                        0x0065 => {
+                            // TODO
+                            self.pc += 2;
+                        }
+                        _ => {
+                            panic!("ERROR: Unknown opcode: {:#X}", self.opcode);
+                        }
+                    }
+                }
+                _ => {
+                    panic!("ERROR: Unknown opcode: {:#X}", self.opcode);
+                }
+            }
+
+            // Update timers
+            if self.delay_timer > 0 {
+                self.delay_timer -= 1;
+            }
+
+            if self.sound_timer > 0 {
+                if self.sound_timer == 1 {
+                    audio_unit.device.resume();
+                    self.sound_timer -= 1;
+                    audio_unit.device.pause();
+                }
+            }
+        }
     }
 
     pub struct Platform {
@@ -77,20 +225,25 @@ pub mod chip_8 {
         }
     }
 
+    // Cartridge Unit
+    pub struct CU {
+        pub buffer: Vec<u8>,
+    }
+
+    impl CU {
+        pub fn new(path: &str) -> Result<CU, ()> {
+            let buffer = fs::read(path).expect("ERROR: Unable to locate ROM");
+            Ok(CU { buffer })
+        }
+    }
+
     // Graphical Unit
     pub struct GU {
         pub canvas: Canvas<sdl2::video::Window>,
     }
 
     impl GU {
-        pub fn new(
-            context: &Sdl,
-            title: &str,
-            window_width: u32,
-            window_height: u32,
-            // texture_width: i32,
-            // texture_height: i32,
-        ) -> GU {
+        pub fn new(context: &Sdl, title: &str, window_width: u32, window_height: u32) -> GU {
             let video_subsystem = context.video().unwrap();
 
             let window = video_subsystem
@@ -193,6 +346,4 @@ pub mod chip_8 {
             false
         }
     }
-
-    // ROM Unit
 }
